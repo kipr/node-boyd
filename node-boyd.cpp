2108eadfadcbbc63b7adbf1685d57091bf2a0ea0
Christoph Heiss <christoph.heiss@hotmail.com>
2015-07-09 22:37:45 +0200

    Port the module functions to node.js v0.12.

--- node-boyd.cpp
+++ node-boyd.cpp
@@ -6,7 +6,6 @@
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/imgproc.hpp>
 
-using namespace v8;
 std::string base64_encode(unsigned char const* , unsigned int len);
 std::string base64_decode(std::string const& s);
 
@@ -15,61 +14,50 @@ std::string base64_decode(std::string const& s);
 // maybe related to http://answers.opencv.org/question/11294/delete-videocapture/
 static cv::VideoCapture cap;
 
-Handle<Value> open(const Arguments &args)
+
+void open(const v8::FunctionCallbackInfo<v8::Value>& info)
 {
-  HandleScope scope;
-  
-  Handle<Object> ret = Object::New();
-  
-  ret->Set(String::New("success"), Boolean::New(cap.open(0)));
-  
-  if(!cap.isOpened())
-  {
-    return scope.Close(ret);
-  }
-  
-  Handle<Object> handle = Object::New();
-  
-  ret->Set(String::New("handle"), handle);
-  
-  return scope.Close(ret);
+  v8::Handle<v8::Object> ret = v8::Object::New(info.GetIsolate());
+  ret->Set(v8::String::NewFromUtf8(info.GetIsolate(), "success"),
+                                   v8::Boolean::New(info.GetIsolate(), cap.open(0)));
+
+  info.GetReturnValue().Set(ret);
 }
 
-Handle<Value> close(const Arguments &args)
+
+void close(const v8::FunctionCallbackInfo<v8::Value>& info)
 {
-  HandleScope scope;
-  
   cap.release();
-  
-  return scope.Close(Boolean::New(true));
+
+  info.GetReturnValue().Set(true);
 }
 
-Handle<Value> getImage(const Arguments &args)
+
+void getImage(const v8::FunctionCallbackInfo<v8::Value>& info)
 {
-  HandleScope scope;
-  
   cv::Mat im;
   cap.read(im);
-  
+
   cv::Mat res;
   cv::resize(im, res, cv::Size(320, 240));
-  
+
   std::vector<int> param;
   param.push_back(cv::IMWRITE_JPEG_QUALITY);
   param.push_back(90);
-  
+
   std::vector<unsigned char> buff(320 * 240);
-  
+
   cv::imencode(".jpg", res, buff, param);
-  
-  return scope.Close(String::New(base64_encode(&buff[0], buff.size()).c_str()));
+
+  info.GetReturnValue().Set(v8::String::NewFromUtf8(info.GetIsolate(),
+                                          base64_encode(&buff[0], buff.size()).c_str()));
 }
 
-void init(Handle<Object> target)
+void init(v8::Handle<v8::Object> exports)
 {
-  target->Set(String::NewSymbol("open"), FunctionTemplate::New(open)->GetFunction());
-  target->Set(String::NewSymbol("close"), FunctionTemplate::New(close)->GetFunction());
-  target->Set(String::NewSymbol("getImage"), FunctionTemplate::New(getImage)->GetFunction());
+  NODE_SET_METHOD(exports, "open", open);
+  NODE_SET_METHOD(exports, "close", close);
+  NODE_SET_METHOD(exports, "getImage", getImage);
 }
 
 NODE_MODULE(node_boyd, init)
